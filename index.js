13b4fae1ccb85c5800b5a7ce94cf7ab8376e1ae1
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-04-30 11:42:41 +0100

    test

--- index.js
+++ index.js
@@ -22,13 +22,12 @@ mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true,  useUnifiedTopol
 require('./models/user')
 require('./models/post')
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')))
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    })
-}
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.use(cors())
 app.use(express.json())
